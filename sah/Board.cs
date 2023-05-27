1c7b722316c9569e95e6954e8860eaee895e3b79
neag darius <dariusstefan.neag@ulbsibiu.com>
2023-05-27 20:21:04 +0300

    added movment to every piece and game starts with white

--- sah/Board.cs
+++ sah/Board.cs
@@ -12,16 +12,26 @@ namespace sah
     {
         private const int BoardSize = 10;
         private const int CellSize = 50;
+       
 
         private readonly Form form;
         private Dictionary<string, Color> pieceColors;
         private Dictionary<string, Point[]> piecePositions;
         private readonly Dictionary<string, Image> pieceImages;
+        private string selectedPiece;
+        private Point selectedPosition;
+        private bool isPieceSelected;
+        private string currentPlayer;
+        private bool canMovePlayer1;
+        private bool canMovePlayer2;
+        private bool canMovePlayer3;
+        private bool canMovePlayer4;
 
 
         public Board(Form form)
         {
             this.form = form;
+            
 
             pieceColors = new Dictionary<string, Color>
 
@@ -72,12 +82,22 @@ namespace sah
 
         };
 
-            
+
 
             InitializePiecePositions();
 
             form.Paint += DrawChessboard;
             form.Paint += DrawPieces;
+            form.MouseClick += HandleMouseClick;
+
+            selectedPiece = string.Empty;
+            selectedPosition = Point.Empty;
+            isPieceSelected = false;
+            currentPlayer = "Player 1";
+            canMovePlayer1 = true;
+            canMovePlayer2 = true;
+            canMovePlayer3 = true;
+            canMovePlayer4 = true;
         }
 
         private void InitializePiecePositions()
@@ -133,6 +153,7 @@ namespace sah
 
                     Brush brush = new SolidBrush(cellColor);
                     g.FillRectangle(brush, x, y, CellSize, CellSize);
+                   
                 }
             }
         }
@@ -147,7 +168,7 @@ namespace sah
                 Point[] positions = kvp.Value;
 
                 Image pieceImage = pieceImages[pieceName];
-                
+
 
                 foreach (Point position in positions)
                 {
@@ -155,8 +176,425 @@ namespace sah
                     int y = position.Y * CellSize;
 
                     g.DrawImage(pieceImage, x, y, CellSize, CellSize);
+
+                }
+            }
+            
+        }
+        private void HandleMouseClick(object sender, MouseEventArgs e)
+        {
+            if (e.Button == MouseButtons.Left)
+            {
+                int row = e.Y / CellSize;
+                int col = e.X / CellSize;
+                Point clickedPosition = new Point(col, row);
+
+                if (!isPieceSelected)
+                {
+                    if (piecePositions.ContainsKey(GetPieceName(clickedPosition)) && GetPieceOwner(clickedPosition) == currentPlayer)
+                    {
+                        selectedPiece = GetPieceName(clickedPosition);
+                        selectedPosition = clickedPosition;
+                        isPieceSelected = true;
+
+                    }
+                }
+                else
+                {
+                    if (IsValidMove(selectedPiece, selectedPosition, clickedPosition))
+                    {
+                        MovePiece(selectedPiece, selectedPosition, clickedPosition);
+                        selectedPiece = string.Empty;
+                        selectedPosition = Point.Empty;
+                        isPieceSelected = false;
+                        currentPlayer = GetNextPlayer();
+                        form.Invalidate();
+
+                    }
                 }
             }
         }
+
+        private bool IsValidMove(string pieceName, Point currentPosition, Point targetPosition)
+        {
+            // Validate the move based on the rules of chess
+            // ...
+            // Implement the specific rules for each type of piece and the interactions in the 4-army chess game
+            // Return true if the move is valid, false otherwise
+            // ...
+            if (pieceName.StartsWith("King"))
+            {
+                return IsKingMoveValid(currentPosition, targetPosition);
+            }
+            if (pieceName.StartsWith("Queen"))
+            {
+                return IsQueenMoveValid(currentPosition, targetPosition);
+            }
+
+            else if (pieceName.StartsWith("Rook"))
+            {
+                return IsRookMoveValid(currentPosition, targetPosition);
+            }
+            else if (pieceName.StartsWith("Bishop"))
+            {
+                return IsBishopMoveValid(currentPosition, targetPosition);
+            }
+            else if (pieceName.StartsWith("Knight"))
+            {
+                return IsKnightMoveValid(currentPosition, targetPosition);
+            }
+            else if (pieceName.StartsWith("Cardinal"))
+            {
+                return IsCardinalMoveValid(currentPosition, targetPosition);
+            }
+            else if (pieceName.StartsWith("Pawn"))
+            {
+                return IsPawnMoveValid(pieceName, currentPosition, targetPosition);
+            }
+          
+
+            return false;
+        }
+
+        private bool IsKingMoveValid(Point currentPosition, Point targetPosition)
+        {
+            int deltaX = Math.Abs(targetPosition.X - currentPosition.X);
+            int deltaY = Math.Abs(targetPosition.Y - currentPosition.Y);
+
+            // The King can move one square in any direction (horizontal, vertical, or diagonal)
+            if ((deltaX == 1 && deltaY == 0) || (deltaX == 0 && deltaY == 1) || (deltaX == 1 && deltaY == 1))
+            {
+                return true; // Valid move
+            }
+
+            return false; // Invalid move
+        }
+        private bool IsQueenMoveValid(Point currentPosition, Point targetPosition)
+        {
+            // Queen movement logic
+            int deltaX = Math.Abs(targetPosition.X - currentPosition.X);
+            int deltaY = Math.Abs(targetPosition.Y - currentPosition.Y);
+
+            // The Queen can move horizontally, vertically, or diagonally any number of squares
+            if ((deltaX > 0 && deltaY == 0) || (deltaX == 0 && deltaY > 0) || (deltaX == deltaY && deltaX > 0))
+            {
+                // Check if there are any pieces blocking the path
+                int startX = currentPosition.X;
+                int startY = currentPosition.Y;
+                int endX = targetPosition.X;
+                int endY = targetPosition.Y;
+
+                int stepX = Math.Sign(endX - startX);
+                int stepY = Math.Sign(endY - startY);
+
+                int x = startX + stepX;
+                int y = startY + stepY;
+
+                while (x != endX || y != endY)
+                {
+                    if (GetPieceName(new Point(x, y)) != null)
+                    {
+                        return false; // Path is blocked
+                    }
+
+                    x += stepX;
+                    y += stepY;
+                }
+
+                return true; // Valid move
+            }
+            return false; // Invalid move
+        }
+
+  
+        
+
+        private bool IsRookMoveValid(Point currentPosition, Point targetPosition)
+        {
+            int deltaX = Math.Abs(targetPosition.X - currentPosition.X);
+            int deltaY = Math.Abs(targetPosition.Y - currentPosition.Y);
+
+            // The Rook can move horizontally or vertically any number of squares, but not diagonally
+            if ((deltaX > 0 && deltaY == 0) || (deltaX == 0 && deltaY > 0))
+            {
+                // Check if there are any pieces blocking the path
+                int startX = currentPosition.X;
+                int startY = currentPosition.Y;
+                int endX = targetPosition.X;
+                int endY = targetPosition.Y;
+
+                if (deltaX > 0) // Horizontal movement
+                {
+                    int stepX = Math.Sign(endX - startX);
+                    for (int x = startX + stepX; x != endX; x += stepX)
+                    {
+                        if (GetPieceName(new Point(x, startY)) != null)
+                        {
+                            return false; // Path is blocked
+                        }
+                    }
+                }
+                else // Vertical movement
+                {
+                    int stepY = Math.Sign(endY - startY);
+                    for (int y = startY + stepY; y != endY; y += stepY)
+                    {
+                        if (GetPieceName(new Point(startX, y)) != null)
+                        {
+                            return false; // Path is blocked
+                        }
+                    }
+                }
+
+                return true; // Valid move
+            }
+
+            return false; // Invalid move
+        }
+
+        private bool IsBishopMoveValid(Point currentPosition, Point targetPosition)
+        {
+            int deltaX = Math.Abs(targetPosition.X - currentPosition.X);
+            int deltaY = Math.Abs(targetPosition.Y - currentPosition.Y);
+
+            // Bishops move diagonally
+            if (deltaX == deltaY && deltaX > 0)
+            {
+                int stepX = Math.Sign(targetPosition.X - currentPosition.X);
+                int stepY = Math.Sign(targetPosition.Y - currentPosition.Y);
+
+                int x = currentPosition.X + stepX;
+                int y = currentPosition.Y + stepY;
+
+                while (x != targetPosition.X && y != targetPosition.Y)
+                {
+                    if (GetPieceName(new Point(x, y)) != null)
+                    {
+                        return false; // Path is blocked
+                    }
+
+                    x += stepX;
+                    y += stepY;
+                }
+
+                return true; // Valid move
+            }
+
+            return false; // Invalid move
+        }
+
+        private bool IsKnightMoveValid(Point currentPosition, Point targetPosition)
+        {
+            int deltaX = Math.Abs(targetPosition.X - currentPosition.X);
+            int deltaY = Math.Abs(targetPosition.Y - currentPosition.Y);
+
+            // Knights move in an L-shape pattern: two squares in one direction and one square perpendicular to it
+            if ((deltaX == 2 && deltaY == 1) || (deltaX == 1 && deltaY == 2))
+            {
+                return true; // Valid move
+            }
+
+            return false; // Invalid move
+        }
+
+       
+
+
+        private bool IsCardinalMoveValid(Point currentPosition, Point targetPosition)
+        {
+            int deltaX = Math.Abs(targetPosition.X - currentPosition.X);
+            int deltaY = Math.Abs(targetPosition.Y - currentPosition.Y);
+
+            // Cardinals move as a bishop or as a knight
+            // Check if the move is valid as a bishop
+            if (deltaX == deltaY && deltaX > 0)
+            {
+                int stepX = Math.Sign(targetPosition.X - currentPosition.X);
+                int stepY = Math.Sign(targetPosition.Y - currentPosition.Y);
+
+                int x = currentPosition.X + stepX;
+                int y = currentPosition.Y + stepY;
+
+                while (x != targetPosition.X && y != targetPosition.Y)
+                {
+                    if (GetPieceName(new Point(x, y)) != null)
+                    {
+                        return false; // Path is blocked
+                    }
+
+                    x += stepX;
+                    y += stepY;
+                }
+
+                return true; // Valid move as a bishop
+            }
+
+            // Check if the move is valid as a knight
+            if ((deltaX == 2 && deltaY == 1) || (deltaX == 1 && deltaY == 2))
+            {
+                return true; // Valid move as a knight
+            }
+
+            return false; // Invalid move
+        }
+
+        private bool IsPawnMoveValid(string pieceName, Point currentPosition, Point targetPosition)
+        {
+            int deltaX = Math.Abs(targetPosition.X - currentPosition.X);
+            int deltaY = Math.Abs(targetPosition.Y - currentPosition.Y);
+
+            // Pawns can move one or two squares forward orthogonally (without jumping)
+            // and capture one square diagonally
+            if ((deltaX == 1 && deltaY == 0) && currentPlayer.StartsWith("Player 1")) // Player 1's pawns can only move forward
+            {
+                if (targetPosition.X < currentPosition.X) // Moving towards the opponent's side
+                {
+                    return true; // Valid move
+                }
+            }
+            else if ((deltaX == 1 && deltaY == 0) && currentPlayer.StartsWith("Player 3")) // Player 3's pawns can only move forward
+            {
+                if (targetPosition.X > currentPosition.X) // Moving towards the opponent's side
+                {
+                    return true; // Valid move
+                }
+            }
+            else if ((deltaX == 0 && deltaY == 1) && (currentPlayer.StartsWith("Player 2") || currentPlayer.StartsWith("Player 4"))) // Player 2 and Player 4's pawns can only move vertically
+            {
+                if (targetPosition.Y < currentPosition.Y) // Moving towards the opponent's side
+                {
+                    return true; // Valid move
+                }
+            }
+            else if (deltaX == 1 && deltaY == 1) // Pawn capture diagonally
+            {
+                string capturedPiece = GetPieceName(targetPosition);
+                if (capturedPiece != null && !capturedPiece.StartsWith(currentPlayer.Substring(0, 6))) // Check if the target position has an opponent's piece
+                {
+                    return true; // Valid capture
+                }
+            }
+
+            return false; // Invalid move
+        }
+        private void MovePiece(string pieceName, Point currentPosition, Point targetPosition)
+        {
+            // Move the piece from the current position to the target position
+            // Update the piecePositions dictionary accordingly
+            // Implement the logic for capturing opponent pieces if necessary
+            // ...
+            if (piecePositions.ContainsKey(pieceName))
+            {
+                Point[] positions = piecePositions[pieceName];
+                for (int i = 0; i < positions.Length; i++)
+                {
+                    if (positions[i] == currentPosition)
+                    {
+                        positions[i] = targetPosition;
+                        break;
+                    }
+                }
+
+                if (pieceName.StartsWith("Pawn") && Math.Abs(targetPosition.Y - currentPosition.Y) == 2)
+                {
+                    // Handle pawn's double move
+                    // ...
+                }
+
+                if (piecePositions.ContainsKey(GetPieceName(targetPosition)))
+                {
+                    // Capture opponent's piece
+                    // ...
+                }
+            }
+        }
+
+        private string GetPieceName(Point position)
+        {
+            foreach (KeyValuePair<string, Point[]> kvp in piecePositions)
+            {
+                Point[] positions = kvp.Value;
+                if (positions.Contains(position))
+                {
+                    return kvp.Key;
+                }
+            }
+            return string.Empty;
+        }
+
+        private string GetPieceOwner(Point position)
+        {
+            foreach (KeyValuePair<string, Point[]> kvp in piecePositions)
+            {
+                Point[] positions = kvp.Value;
+                if (positions.Contains(position))
+                {
+                    string pieceName = kvp.Key;
+                    if (pieceName.EndsWith("Yellow"))
+                    {
+                        return "Player 1";
+                    }
+                    else if (pieceName.EndsWith("Black"))
+                    {
+                        return "Player 2";
+                    }
+                    else if (pieceName.EndsWith("Red"))
+                    {
+                        return "Player 3";
+                    }
+                    else if (pieceName.EndsWith("Green"))
+                    {
+                        return "Player 4";
+                    }
+                }
+            }
+            return string.Empty;
+        }
+
+        private string GetNextPlayer()
+        {
+            if (currentPlayer == "Player 1")
+            {
+                if (canMovePlayer2)
+                    return "Player 2";
+                else if (canMovePlayer3)
+                    return "Player 3";
+                else if (canMovePlayer4)
+                    return "Player 4";
+            }
+            else if (currentPlayer == "Player 2")
+            {
+                if (canMovePlayer3)
+                    return "Player 3";
+                else if (canMovePlayer4)
+                    return "Player 4";
+                else if (canMovePlayer1)
+                    return "Player 1";
+            }
+            else if (currentPlayer == "Player 3")
+            {
+                if (canMovePlayer4)
+                    return "Player 4";
+                else if (canMovePlayer1)
+                    return "Player 1";
+                else if (canMovePlayer2)
+                    return "Player 2";
+            }
+            else if (currentPlayer == "Player 4")
+            {
+                if (canMovePlayer1)
+                    return "Player 1";
+                else if (canMovePlayer2)
+                    return "Player 2";
+                else if (canMovePlayer3)
+                    return "Player 3";
+            }
+
+            return currentPlayer;
+        }
+
     }
 }
+
+    
+
